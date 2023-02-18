4eca44a4484313c42e294ebc48995671b6197892
vekoada <adam@vekony.com>
2023-02-17 16:31:42 -0800

    Use chunker to break summary job into small parts

--- summarizer.py
+++ summarizer.py
@@ -1,9 +1,10 @@
 import openai
 import json
-from pytube import YouTube
+#from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi as yt
 from youtube_transcript_api._errors import TranscriptsDisabled
 from VidID import extract
+from chunker import create_chunks
 
 def get_keys(path):
     with open(path) as f:
@@ -16,8 +17,8 @@ openai.api_key = get_keys('c:/Users/Adam/repos/summary-generator/.secret/keys.js
 url = 'https://www.youtube.com/watch?v=4x7MkLDGnu8'
 video_id = extract(url)
 
-video = YouTube(url=url)
-video_title = video.title
+#video = YouTube(url=url)
+#video_title = video.title
 
 try:
     #Grab list of dictionaries 'subs'
@@ -28,13 +29,33 @@ try:
     for i in subs:
         text+= '{} '.format(i['text'])
 
-    #Initialize prompt with instructions and video text
-    prompt = (f"Please summarize this text: {text}")
+    chunk_list = create_chunks(text, 800)
+    summary_list = []
 
+    for chunk in chunk_list:
+
+        #Initialize prompt with instructions and video text
+        prompt = (f"Please summarize this section of video captions, keeping an eye out for the main points: {chunk}")
+
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=prompt,
+            max_tokens=200,
+            temperature=0.5,
+            n=1)
+
+        chunk_summary = response.choices[0].text
+        summary_list.append(chunk_summary)
+
+    summary_concatenated = ""
+    for chunk_summary in summary_list:
+        summary_concatenated+= chunk_summary
+
+    prompt = (f"Please summarize this video, based on the following description. Pay attention to the main points, and strive for accuracy: {summary_concatenated}")
     response = openai.Completion.create(
-        engine="text-curie-001",
+        engine="text-davinci-002",
         prompt=prompt,
-        max_tokens=200,
+        max_tokens=750,
         temperature=0.5,
         n=1)
 
