a86be78b832a3e22aec07273e27b315d53596265
vekoada <adam@vekony.com>
2023-02-28 17:10:59 -0800

    Create function to extract text from site url

---
+++ website_parser.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+def website_parser(url):  
+    # Send a request to the website and get the response
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        
+        # Extract the text content of the website
+        page_content = response.text
+        soup = BeautifulSoup(page_content, 'html.parser')
+
+        text = ''
+        for data in soup.find_all("p"): 
+            text+= ' ' + data.get_text()
+        return text
+
+    else:
+        return "Error: Could not retrieve website content"
