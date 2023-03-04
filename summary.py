49867753fb48a5c708976f6cb87a3aa7b7581ba3
vekoada <adam@vekony.com>
2023-03-04 10:06:28 -0800

    Switch davinci to gpt-3.5-turbo

--- summary.py
+++ summary.py
@@ -2,6 +2,7 @@ import openai
 from chunker import create_chunks
 from configure import get_credentials
 
+
 def run(text, chunk_size=800, path_to_credentials='c:/Users/Adam/repos/summary-generator/.secret/keys.json'):
     chunk_list = create_chunks(text, chunk_size)
     summary_list = []
@@ -11,30 +12,21 @@ def run(text, chunk_size=800, path_to_credentials='c:/Users/Adam/repos/summary-g
     for chunk in chunk_list:
 
         #Initialize prompt with instructions and video text
-        prompt = (f"Please summarize this video, article, or pdf, based on the following text: {chunk}")
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages = [{"role": "user", "content" : f"Please summarize this video, article, or pdf, based on the following text in 3 sentences or less: {chunk}" }])
 
-        response = openai.Completion.create(
-            engine="gpt-3.5-turbo",
-            prompt=prompt,
-            max_tokens=300,
-            temperature=0.5,
-            n=1)
-
-        chunk_summary = response.choices[0].text
+        chunk_summary = response['choices'][0]['message']['content']
         summary_list.append(chunk_summary)
 
     summary_concatenated = ""
     for chunk_summary in summary_list:
         summary_concatenated+= ' ' + chunk_summary
 
-    prompt = (f"Please summarize this video, based on the following description. Strive for accuracy and limit yourself to 450 words: {summary_concatenated}")
-    response = openai.Completion.create(
-    engine="gpt-3.5-turbo",
-        prompt=prompt,
-        max_tokens=1500,
-        temperature=0.5,
-        n=1)
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+         messages=[{"role": "user", "content": f"Please summarize this video, based on the following description. Strive for accuracy: {summary_concatenated}"}])
 
-    summary = response.choices[0].text
+    summary = response['choices'][0]['message']['content']
 
-    return summary
+    return summary, summary_concatenated
