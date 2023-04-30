031a95285bd2ec0327c40f257c13a39c46db5869
vekoada <adam@vekony.com>
2023-04-30 12:20:40 -0700

    Add overlap to reg chunker; add random_chunks

--- chunker.py
+++ chunker.py
@@ -1,7 +1,11 @@
-def create_chunks(text, chunk_size):
+from random import randint
+from math import log
+
+def create_chunks(text, overlap):
+    chunk_size = 900 - overlap
     #Set number of chunks
     num_chunks = (len(text) + chunk_size) // chunk_size
-    chunk_list = [] #List of chunks
+    chunk_list = []
 
     #Loop over chunks
     start = 0
@@ -13,7 +17,35 @@ def create_chunks(text, chunk_size):
             if end == -1:
                 #No whitespace; use original end index
                 end = start + chunk_size
-        chunk_list.append(text[start:end])
+        #Include overlap if not first chunk
+        if i > 0:
+            overlap_start = start - overlap
+            chunk_list.append(text[overlap_start:end])
+            print(text[overlap_start:end])
+            print("-------------------")
+        else:
+            chunk_list.append(text[start:end])
+            print(text[start:end])
+            print("-------------------")
         start = end + 1
+
+    return chunk_list
+
+def random_chunks(text):
+    chunk_size = 900
+    chunk_list = []
+    
+    num_chunks = int(round((log(((len(text) + chunk_size)/1000.0)**3) + 1), 0)) 
+    
+    for i in range(num_chunks):
+        start = randint(0, (len(text) - 1 - 900))
+        end = min(start + chunk_size, len(text))
+        if end < len(text) and not text[end].isspace():
+            #Adjust end index to stop on whitespace
+            end = text.rfind(' ', start, end)
+            if end == -1:
+                #No whitespace; use original end index
+                end = start + chunk_size
+        chunk_list.append(text[start:end])
 
     return chunk_list
