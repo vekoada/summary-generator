2e57018ad3f5706573432dda949a19a8f5a578a2
vekoada <adam@vekony.com>
2023-09-19 09:43:20 -0700

    Write docstrings for video functions

--- video.py
+++ video.py
@@ -7,10 +7,33 @@ import configure
 
 openai.api_key = configure.get_credentials('c:/Users/Adam/repos/summary-generator/.secret/keys.json')['openai']
 
+def extract_id(url: str) -> str:
+    """
+    Extracts the video ID from a YouTube video URL.
 
-def extract_id(url):
-    
+    This function takes a YouTube video URL as input and extracts the unique video ID from it. The URL must be from
+    the YouTube.com domain for the function to work properly. If the URL is invalid or not from YouTube.com, an error
+    message is returned.
+
+    Args:
+        url (str): A string containing the YouTube video URL.
+
+    Returns:
+        str: The extracted video ID if the URL is valid and contains a video ID.
+        str: An error message if the URL is not a video or not from the YouTube.com domain.
+
+    Example:
+        >>> url = "https://www.youtube.com/watch?v=abcdef12345"
+        >>> extract_id(url)
+        'abcdef12345'
+
+    Note:
+        This function assumes that the provided URL is a valid YouTube video URL and follows the standard format
+        with the 'v' parameter containing the video ID.
+
+    """
     error = "The url provided is not a video, or is not from the YouTube.com domain"
+    
     # Check if the URL is from the YouTube.com domain
     if "youtube.com" not in url:
         return error
@@ -27,15 +50,41 @@ def extract_id(url):
         return video_id
         
     
-def get_text(url):
-    video_id = extract_id(url)
-    #Grab list of dictionaries 'subs'
-    subs = yt.get_transcript(video_id=video_id,languages=['en'])
+def get_text(url: str) -> str:
+    """
+    Retrieves the transcribed text from a YouTube video.
 
-    #Build text string
+    This function takes a YouTube video URL as input, extracts the video ID, and fetches the transcribed text of the video
+    in English (assuming English is one of the available languages). The transcribed text is returned as a single string.
+
+    Args:
+        url (str): A string containing the YouTube video URL.
+
+    Returns:
+        str: The transcribed text of the video as a single string.
+
+    Example:
+        >>> url = "https://www.youtube.com/watch?v=abcdef12345"
+        >>> get_text(url)
+        'This is the transcribed text of the video.'
+
+    Note:
+        This function relies on the 'extract_id' function to extract the video ID from the URL. It also assumes that the
+        YouTube video has English subtitles available.
+
+    """
+    # Extract the video ID from the URL
+    video_id = extract_id(url)
+
+    # Fetch the subtitles for the video in English
+    subs = yt.get_transcript(video_id=video_id, languages=['en'])
+
+    # Build a single text string from the list of subtitle dictionaries
     text = ''
-    for i in subs:
-        text+= '{} '.format(i['text'])
-        
+    for subtitle in subs:
+        text += '{} '.format(subtitle['text'])
+
     return text
+
     
+#print(get_text("https://www.youtube.com/watch?v=imAYfKW1WG8"))
