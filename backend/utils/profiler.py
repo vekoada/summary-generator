a77e43de2d55e28a09a4988a12bb01ab345fe45f
vekoada <adam@vekony.com>
2024-08-10 17:41:41 -0700

    Reshuffle directories

---
+++ backend/utils/profiler.py
@@ -0,0 +1,43 @@
+########################################################
+# Performance profiling - https://realpython.com/python-profiling/
+########################################################
+
+from cProfile import Profile
+from pstats import SortKey, Stats
+
+# Local imports
+from sync_summary import summarize
+from async_summary import async_summarize
+from video import get_transcript
+
+def log_profile(mode: str): 
+    with Profile() as profile:
+
+        # Open log file in write mode
+        with open(f"src/utils/logs/gpt-4o-mini_{mode}_summary_profiling.log", 'w') as log_file:
+
+            import sys
+            original_stdout = sys.stdout
+            sys.stdout = log_file # redirect stdout to log file
+
+            text = get_transcript('https://www.youtube.com/watch?v=imAYfKW1WG8')
+            args = [text, 'video', 'openai'] # args to unpack for summarize()
+
+            if mode != "async":
+                summarize(*args) # unpack args
+            else:
+                async_summarize(*args) 
+                
+            stats = Stats(profile)  # profile stats
+            stats.strip_dirs()  # strip directories
+            stats.sort_stats(SortKey.TIME)  # sort by time
+
+            print(f"Profile: {profile}")
+            stats.print_stats()  # print stats
+            
+            # restore stdout
+            sys.stdout = original_stdout
+
+# Run profiling
+log_profile("async") 
+log_profile("regular")
