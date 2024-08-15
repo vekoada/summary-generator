27d84fc9ee2732db6afcafed2aa1cb00fb8b68a5
vekoada <adam@vekony.com>
2024-08-14 20:21:37 -0700

    Add chunk_size, overlap to profiler

--- backend/utils/profiler.py
+++ backend/utils/profiler.py
@@ -10,18 +10,18 @@ from sync_summary import summarize
 from async_summary import async_summarize
 from video import get_transcript
 
-def log_profile(mode: str, api: str):
+def profile_summary(mode: str, api: str, chunk_size: int = 1800, overlap: int = 50):
     with Profile() as profile:
 
         # Open log file in write mode
-        with open(f"src/utils/logs/{api}_{mode}_1800_summary_profiling.log", 'w') as log_file:
+        with open(f"src/utils/logs/{api}_{mode}_{chunk_size}_{overlap}_summary_profiling.log", 'w') as log_file:
 
             import sys
             original_stdout = sys.stdout
             sys.stdout = log_file # redirect stdout to log file
 
             text = get_transcript('https://www.youtube.com/watch?v=imAYfKW1WG8')
-            args = [text, 'video', api] # args to unpack for summarize()
+            args = [text, 'video', api, chunk_size, overlap] # args to unpack for summarize()
 
             if mode != "async":
                 summarize(*args) # unpack args
@@ -39,5 +39,5 @@ def log_profile(mode: str, api: str):
             sys.stdout = original_stdout
 
 # Run profiling
-log_profile("async", "groq") 
-log_profile("regular", "groq")
+profile_summary("async", "groq", 1800, 50) 
+profile_summary("regular", "groq", 1800, 50)
