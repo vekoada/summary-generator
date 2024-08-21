b20cbfedc3116f09ffb40b7fbe62a77a7a4da814
vekoada <adam@vekony.com>
2024-08-20 21:29:01 -0700

    Create transcribe function (not working!)

---
+++ backend/utils/transcribe.py
@@ -0,0 +1,40 @@
+# Standard library imports
+import os
+
+# Third party imports
+from deepgram import DeepgramClient, PrerecordedOptions
+import asyncio
+
+# Local imports
+from configure import get_credentials
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+key = get_credentials(path + '/.secret/keys.json')['Deepgram']
+
+async def transcribe(url: str) -> str:
+    AUDIO_URL = {
+        "url": url
+    }
+
+    try:
+        client = DeepgramClient(key)
+
+        options = PrerecordedOptions(
+            smart_format=True,
+            summarize="v2",
+        )
+        url_response = await client.listen.asyncrest.v("1").transcribe_url(
+            AUDIO_URL, options
+        )
+
+        load = await url_response.to_json
+
+        return load
+    
+    except Exception as e:
+        print(f"Exception: {e}")
+
+response = asyncio.run(transcribe(url='https://youtu.be/9en2brDsbH4')) 
+# DOES NOT work with YouTube URLs. They have explicit protections from this. A workaround would be to download MP3 and then transcribe. Will figure out later.
+# Doesn't work with Facebook, either. 
